f5906e85013659ce9aacd2cf6369bc4cb08f8c57
janthony25 <junanthony.business@gmail.com>
2024-02-08 21:49:21 +1300

    Add project files.

---
+++ Zoo-App/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+// need to add to reference first before you can use
+using System.Configuration;
+
+namespace Zoo_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // setting up connection string
+            string connectionString = ConfigurationManager.ConnectionStrings["Zoo-App.Properties.Settings.ZooDBConnectionString"].ConnectionString;
+        }
+    }
+}
